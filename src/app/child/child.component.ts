3acfaceb49bff35b9bcbe5af5f4379eb1465d2eb
karthigayan95 <35838536+karthigayan95@users.noreply.github.com>
2022-12-23 11:45:17 +0530

    1 file modified, 1 added

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
+  @Input() name: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
