0a00931cb7f0e4f2632e1d8e93e18e6bb3c5b96c
karthigayan95 <35838536+karthigayan95@users.noreply.github.com>
2022-12-23 11:29:26 +0530

    2 files modified

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent implements OnInit {
+  name: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
